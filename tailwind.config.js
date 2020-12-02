569a5bb34f94ea052fe41622c1ff6321df93e0ed
oluwafemiomiyefa <oluwafemiomiyefa@gmail.com>
2020-12-02 11:57:24 +0100

    img progress

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,10 @@
 module.exports = {
 	purge: [],
 	theme: {
-		height: {
-  +		sm: '8px',
-  +     md: '16px',
-  +     lg: '24px',
-  +     xl: '48px',
-		}
-	 }
-}
+	
 		extend: {},
 	},
 	variants: {
-		extend: {
-	+	height: ['hover', 'focus'],
 		backgroundColor: ["responsive", "hover", "focus", "active"],
 		textColor: ["responsive", "hover", "focus", "active"],
 	},
