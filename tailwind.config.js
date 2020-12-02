1bb98762edbe5b862773625efa122a45ffb704be
oluwafemiomiyefa <oluwafemiomiyefa@gmail.com>
2020-12-02 11:26:16 +0100

    addded img

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,19 @@
 module.exports = {
 	purge: [],
 	theme: {
+		height: {
+  +		sm: '8px',
+  +     md: '16px',
+  +     lg: '24px',
+  +     xl: '48px',
+		}
+	 }
+}
 		extend: {},
 	},
 	variants: {
+		extend: {
+	+	height: ['hover', 'focus'],
 		backgroundColor: ["responsive", "hover", "focus", "active"],
 		textColor: ["responsive", "hover", "focus", "active"],
 	},
